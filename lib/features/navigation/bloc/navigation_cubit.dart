ec6045acf7b0e62cc4cbe446e528041923895020
Daniel <thecodedaniel@gmail.com>
2025-02-14 23:25:50 +0100

    feat: settings view

--- lib/features/navigation/bloc/navigation_cubit.dart
+++ lib/features/navigation/bloc/navigation_cubit.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:socket_probe/features/dashboard/presentation/screens/dashboard_view.dart';
+import 'package:socket_probe/features/settings/presentation/screens/settings_view.dart';
 
 class NavigationCubit extends Cubit<int> {
   NavigationCubit() : super(0);
 
   List<Widget> navigationPages = [
     DashboardView(),
-    Container(),
+    SettingsView(),
     Container(),
   ];
 
